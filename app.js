0797b5e8fcfa72492bace5ac70ac2bf7ebd23cbb
Joyce <joycelin79@gmail.com>
2021-11-24 14:36:57 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict'
+
+function createEnv() {
+    var secretString = document.getElementById('secret-string').value,
+        runButtonIndex = 0,
+        envData = {
+            password: secretString
+        };
+    _pm('env.create', 'Secret Strings', envData, runButtonIndex);
+    return false;
+}
+
+(function (p, o, s, t, m, a, n) {
+    !p[s] && (p[s] = function () { (p[t] || (p[t] = [])).push(arguments); });
+    !o.getElementById(s + t) && o.getElementsByTagName("head")[0].appendChild((
+        (n = o.createElement("script")),
+        (n.id = s + t), (n.async = 1), (n.src = m), n
+    ));
+}(window, document, "_pm", "PostmanRunObject", "https://run.pstmn.io/button.js"));
